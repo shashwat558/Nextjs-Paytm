10dddb3b6db74664878f6c99d0c1223cc67ebf08
shashwat558 <tumbaktuhere@gmail.com>
2024-08-01 23:54:07 +0530

    added appbar and sidebar component, created dashboard folder

---
+++ apps/user-app/components/SidebarItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { usePathname, useRouter } from 'next/navigation';
+
+const SidebarItem = ({href, title, icon}: {href:string; title:string; icon:React.ReactNode}) => {
+  return (
+    <div>SidebarItem</div>
+  )
+}
+
+export default SidebarItem
