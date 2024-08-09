166b28a4230432fd085b0948445afbe30e6204b6
shashwat558 <tumbaktuhere@gmail.com>
2024-08-09 23:37:51 +0530

    Added P2P transaction components

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SidebarItem } from '../../components/SidebarItem';
+import { title } from 'process';
 
 
 function layout({
@@ -14,6 +15,7 @@ function layout({
                 <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
                 <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
                 <SidebarItem href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
+                <SidebarItem href={'/p2ptransaction'} icon={<P2PTransferIcon /> }title='P2Ptransation'/>
             </div>
         </div>
             {children}
@@ -37,6 +39,11 @@ function TransactionsIcon() {
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
   </svg>
   
+}
+function P2PTransferIcon() {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+  </svg>
 }
 
 export default layout
