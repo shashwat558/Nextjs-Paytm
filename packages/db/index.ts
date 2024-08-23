6f8d04d3c91c005d9bd3eccd222927e6293f91f4
shashwat558 <tumbaktuhere@gmail.com>
2024-08-23 22:49:42 +0530

    fixed an issue

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -10,6 +10,7 @@ declare global {
 
 const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+export default prisma
